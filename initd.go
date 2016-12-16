c1d2e2013d89c1d8734caf3ef693de1bae17b4c8
denish <denishorobtsov@gmail.com>
2016-12-16 23:34:44 +0300

    Added magnificent:) event manager.

--- initd.go
+++ initd.go
@@ -2,28 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/fsouza/go-dockerclient"
+	"time"
+	events "github.com/gninjava/registryd/eventmanager"
 )
 
 func main() {
-	endpoint := "unix:///var/run/docker.sock"
-
-	client, err := docker.NewClient(endpoint)
-	if err != nil {
-		panic(err)
-	}
-
-	containers, err := client.ListContainers(docker.ListContainersOptions{All: false})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, c := range containers {
-		fmt.Println("ID:          ", c.ID)
-		fmt.Println("Image:       ", c.Image)
-		fmt.Println("Created:     ", c.Created)
-		fmt.Println("Ports:       ", c.Ports)
-		fmt.Println("Networks:    ", c.Networks)
-		fmt.Println("Command:     ", c.Command)
+	for {
+		events.Listen()
+		fmt.Println("scanning...")
+		time.Sleep(5 * time.Second)
 	}
 }
+
